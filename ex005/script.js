179bce4185f3eaf0d3c2896f01f5b0ee4214e92d
tonetlucas <tonetlucas12@gmail.com>
2022-09-28 23:01:09 -0300

    novos arquivos

---
+++ ex005/script.js
@@ -0,0 +1,12 @@
+var livro = new Object();
+livro.titulo = "Ajax com Jquery";
+livro.autor = "Maurício Samy Silva";
+livro.paginas = 432;
+livro.freteSedex = function (ceporigem, cepdestino, peso){
+    var valorFrete = "";
+    // calcular valor frete
+    return valorFrete
+}
+var nomeAutor = livro.autor;
+var valorFrete = livro.freteSedex(22050047, 69190120, 1060);
+alert("Autor:" + nomeAutor + "\nValor Frete:" + valorFrete);
